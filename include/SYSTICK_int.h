bda25d3289d586ac766b4e81df900593c63f52fd
TaqiEldeen <takeycoo@gmail.com>
2023-06-24 20:35:06 +0300

    Update SYSTICK commenting and add functions

--- include/SYSTICK_int.h
+++ include/SYSTICK_int.h
@@ -8,53 +8,77 @@
 #ifndef SYSTICK_INT_H
 #define SYSTICK_INT_H
 
-/**********************************************************************************************************
- * Description : Interface Function to Initialize the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Initialize the SYSTICK
+ * 
+ * @note This Function Must be called before any other function in this module
+ *       Initialize the SYSTICK with the configured values in the configuration file
+ * 
+ */
 void SYSTICK_vInit(void);
 
-/**********************************************************************************************************
- * Description : Interface Function to Set the Reload Value
- * Outputs     : void
- * Inputs      : Reload Value
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Set the Reload Value
+ * 
+ * @param A_u32Ticks 
+ */
 void SYSTICK_vSetReload(u32 A_u32Ticks);
 
-/**********************************************************************************************************
- * Description : Interface Function to Get the Reload Value
- * Outputs     : Reload Value
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to get the Elapsed Ticks
+ * 
+ * @return u32 
+ */
 u32 SYSTICK_u32GetElapsedTicks(void);
 
-/**********************************************************************************************************
- * Description : Interface Function to Get the Remaining Ticks
- * Outputs     : Remaining Ticks
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Get the Remaining Ticks
+ * 
+ * @return u32 
+ */
 u32 SYSTICK_u32GetRemainingTicks(void);
 
-/**********************************************************************************************************
- * Description : Interface Function to turn off the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Turn off the SYSTICK
+ * 
+ */
 void SYSTICK_vTurnOff(void);
 
-/**********************************************************************************************************
- * Description : Interface Function to turn on the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Turn on the SYSTICK
+ * 
+ */
 void SYSTICK_vTurnOn(void);
 
-/**********************************************************************************************************
- * Description : Interface Function to Set the Call Back Function
- * Outputs     : void
- * Inputs      : Pointer to the Call Back Function
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Set the CallBack Function
+ * 
+ * @param A_ptr_func 
+ */
 void SYSTICK_vSetCallBack(ptr_func_t A_ptr_func);
+
+/**
+ * @brief Interface Function to Set the Interval for only one time
+ * 
+ * @param A_u32Ticks the ticks needed for this interval
+ * @param A_ptr_func the function to be called after this interval
+ */
+void SYSTICK_vSetIntervalSingle(u32 A_u32Ticks, ptr_func_t A_ptr_func);
+
+/**
+ * @brief Interface Function to Set the Interval for periodic calls
+ * 
+ * @param A_u32Ticks the ticks needed for this interval
+ * @param A_ptr_func the function to be called after this interval
+ */
+void SYSTICK_vSetIntervalPeriodic(u32 A_u32Ticks, ptr_func_t A_ptr_func);
+
+/**
+ * @brief Interface Function to set a busy wait for a specific time
+ * 
+ * @param A_u32Ticks the ticks needed for this busy wait
+ */
+void SYSTICK_vSetBusyWait(u32 A_u32Ticks);
+
 
 #endif // SYSTICK_INT_H
--- src/SYSTICK_prg.c
+++ src/SYSTICK_prg.c
@@ -16,84 +16,134 @@
 #include "SYSTICK_int.h"
 #include "SYSTICK_reg.h"
 
+/**
+ * @brief Global Variable to Hold the CallBack Function
+ * 
+ */
 static ptr_func_t SYSTICK_CallBack = ADDRESS_NULL;
 
-/**********************************************************************************************************
- * Description : Interface Function to Initialize the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Global Variable to Hold the state of the SYSTICK in Async mode
+ * 
+ */
+static u8 isSingleInterval = FALSE;
+
+/**
+ * @brief Interface Function to Initialize the SYSTICK
+ * 
+ * @note To enable the SYSTICK you can call the SYSTICK_vTurnOff() function
+ */
 void SYSTICK_vInit(void) {
-    SYSTICK-> CTRL = 0;                       /* Clear CTRL */
-    SYSTICK-> LOAD = SYSTICK_RELOAD_VALUE;    /* Set the maximum value */   
-    SYSTICK-> VAL  = 0;                       /* Clear the current value */
-    SYSTICK-> CTRL |= (SYSTICK_ENABLE << ENABLE)              /* Enable the counter */
-                    | (SYSTICK_INTERRUPT << TICKINT)          /* Enable the interrupt */
-                    | (SYSTICK_CLKSOURCE << CLKSOURCE);       /* Select the clock source */
+    SYSTICK-> CTRL = 0;                                       /* Clear CTRL */
+    SYSTICK-> LOAD = SYSTICK_RELOAD_VALUE;                    /* Set the maximum value */   
+    SYSTICK-> VAL  = 0;                                       /* Clear the current value */
+    SYSTICK-> CTRL |= (SYSTICK_CLKSOURCE << CLKSOURCE);       /* Select the clock source */
 }
 
-/**********************************************************************************************************
- * Description : Interface Function to Turn on the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Turn on the SYSTICK
+ * 
+ */
 void SYSTICK_vTurnOn(void) {
     SET_BIT(SYSTICK-> CTRL, ENABLE);
 }
 
-/**********************************************************************************************************
- * Description : Interface Function to Turn off the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief  Interface Function to Turn off the SYSTICK
+ * 
+ */
 void SYSTICK_vTurnOff(void) {
     CLR_BIT(SYSTICK-> CTRL, ENABLE);
 }
 
-/**********************************************************************************************************
- * Description : Interface Function to Set the Reload Value
- * Outputs     : void
- * Inputs      : Reload Value
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Set the Reload Value
+ * 
+ * @param A_u32Ticks 
+ */
 void SYSTICK_vSetReload(u32 A_u32Ticks){
     SYSTICK-> LOAD = A_u32Ticks;
 }
 
-/**********************************************************************************************************
- * Description : Interface Function to Get the Reload Value
- * Outputs     : Reload Value
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Get the Elapsed Ticks
+ * 
+ * @return u32 
+ */
 u32 SYSTICK_u32GetElapsedTicks(void) {
     return ( (SYSTICK->LOAD) - (SYSTICK-> VAL) );
 }
 
-/**********************************************************************************************************
- * Description : Interface Function to Get the Remaining Ticks
- * Outputs     : Remaining Ticks
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Get the Remaining Ticks
+ * 
+ * @return u32 
+ */
 u32 SYSTICK_u32GetRemainingTicks(void) {
     return (SYSTICK-> VAL);
 }
 
-/**********************************************************************************************************
- * Description : Interface Function to Set the Call Back Function
- * Outputs     : void
- * Inputs      : Pointer to the Call Back Function
- ***********************************************************************************************************/
-void SYSTICK_vSetCallBack(ptr_func_t A_ptr_func){
-    SYSTICK_CallBack = A_ptr_func;
+/**
+ * @brief Interface Function to Set the Interval for only one time
+ * 
+ * @param A_u32Ticks the ticks needed for this interval
+ * @param A_ptr_func the function to be called after this interval
+ */
+void SYSTICK_vSetIntervalSingle(u32 A_u32Ticks, ptr_func_t A_ptr_func){
+    SYSTICK-> CTRL |= (1 << TICKINT);          /* Enable the interrupt */
+    SYSTICK-> LOAD = A_u32Ticks;               /* Set the maximum value */
+    SYSTICK_CallBack = A_ptr_func;             /* Set the CallBack Function */
+    isSingleInterval = TRUE;                   /* Set the state to single interval */
+    SYSTICK_vTurnOn();                         
 }
 
-/**********************************************************************************************************
- * Description : Interrupt Service Routine for the SYSTICK
- * Outputs     : void
- * Inputs      : void
- ***********************************************************************************************************/
+/**
+ * @brief Interface Function to Set the Interval for periodic calls
+ * 
+ * @param A_u32Ticks the ticks needed for this interval
+ * @param A_ptr_func the function to be called after this interval
+ */
+void SYSTICK_vSetIntervalPeriodic(u32 A_u32Ticks, ptr_func_t A_ptr_func){
+    SYSTICK-> CTRL |= (1 << TICKINT);          /* Enable the interrupt */
+    SYSTICK-> LOAD = A_u32Ticks;               /* Set the maximum value */
+    SYSTICK_CallBack = A_ptr_func;             /* Set the CallBack Function */
+    isSingleInterval = FALSE;                  /* Set the state to single interval */
+    SYSTICK_vTurnOn();
+}
+
+/**
+ * @brief Interface Function to set a busy wait for a specific time
+ * 
+ * @param A_u32Ticks the ticks needed for this busy wait
+ */
+void SYSTICK_vSetBusyWait(u32 A_u32Ticks){
+    SYSTICK-> CTRL &= ~(1 << TICKINT);          /* Disable the interrupt */
+    SYSTICK-> LOAD = A_u32Ticks;                /* Set the maximum value */
+    SYSTICK_vTurnOn();
+    while (GET_BIT(SYSTICK-> CTRL, COUNTFLAG) == 0)
+    {
+        /* Wait for time to pass */
+    }
+    SYSTICK_vTurnOff();
+}
+
+/**
+ * @brief ISR Function to Handle the SYSTICK Interrupt
+ * 
+ *  */
 void SysTick_Handler(void) {
     if (SYSTICK_CallBack != ADDRESS_NULL) {
         SYSTICK_CallBack();
+
+        if(isSingleInterval) {
+            isSingleInterval = FALSE;
+            SYSTICK-> CTRL &= ~(1 << TICKINT);          /* Disable the interrupt */
+            SYSTICK_vTurnOff();
+        } 
+        else {
+            /*  Do Nothing  */
+        }
+        
     } else {
         /*  Do Nothing  */
     }
